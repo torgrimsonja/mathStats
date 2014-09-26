c3206edcf79e2b1c0b9cf74660c1744aa4d9e1ce
Joel Christensen <christensenjo14@gmail.com>
2014-09-26 14:40:55 -0600

    PHP...

--- serverSide.php
+++ serverSide.php
@@ -2,27 +2,20 @@
 	//The purpose of this file is to allow the MathStats App to save CSV files so that charts may be recreated with them at a later time
 	
 	//Get file from html form upload
-		$allowedExts = array("gif", "jpeg", "jpg", "png");
-		$temp = explode(".", $_FILES["file"]["name"]);
-		$extension = end($temp);
 		
-		if ((($_FILES["file"]["type"] == "image/gif"))
-		&& ($_FILES["file"]["size"] < 20000)
-		&& in_array($extension, $allowedExts)) {
-		  if ($_FILES["file"]["error"] > 0) {
-			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-		  } else {
-			if (file_exists("upload/" . $_FILES["file"]["name"])) {
-			  echo $_FILES["file"]["name"] . " already exists. ";
-			} else {
-			  move_uploaded_file($_FILES["file"]["tmp_name"],
-			  "upload/" . $_FILES["file"]["name"]);
-			  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-			}
-		  }
-		} else {
-		  echo "Invalid file";
-		}
+		if ($_FILES["file"]["type"] == "csv"){
+			//Make sure the file type is csv
+			  if (file_exists("upload/" . $_FILES["file"]["name"])) {
+					  echo $_FILES["file"]["name"] . " already exists. ";
+				} else {
+					  move_uploaded_file($_FILES["file"]["tmp_name"],
+					  "upload/" . $_FILES["file"]["name"]);
+					  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+				}
+		 } else {
+			//Deal with non-csv files
+			echo "Invalid file";
+		 }
 	//Read file and create a title from it based on a set of conditions
 	
 	//Save the file in a common folder with the generated name
