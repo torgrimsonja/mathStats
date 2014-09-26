d40d8765f6ce9335d41eb0a394f0baa65274da31
Joel Christensen <christensenjo14@gmail.com>
2014-09-26 14:40:10 -0600

    Index Graph Container and PHP Stuff

--- serverSide.php
+++ serverSide.php
@@ -2,15 +2,27 @@
 	//The purpose of this file is to allow the MathStats App to save CSV files so that charts may be recreated with them at a later time
 	
 	//Get file from html form upload
-	if($_FILES["file"]["error"] > 0){
-		  //Set up file upload error reporting
-		  echo "Error: " . $_FILES["file"]["error"] . "<br />";
-	}else{
-		  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-		  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-		  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-		  echo "Stored in: " . $_FILES["file"]["tmp_name"];
-	}
+		$allowedExts = array("gif", "jpeg", "jpg", "png");
+		$temp = explode(".", $_FILES["file"]["name"]);
+		$extension = end($temp);
+		
+		if ((($_FILES["file"]["type"] == "image/gif"))
+		&& ($_FILES["file"]["size"] < 20000)
+		&& in_array($extension, $allowedExts)) {
+		  if ($_FILES["file"]["error"] > 0) {
+			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+		  } else {
+			if (file_exists("upload/" . $_FILES["file"]["name"])) {
+			  echo $_FILES["file"]["name"] . " already exists. ";
+			} else {
+			  move_uploaded_file($_FILES["file"]["tmp_name"],
+			  "upload/" . $_FILES["file"]["name"]);
+			  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+			}
+		  }
+		} else {
+		  echo "Invalid file";
+		}
 	//Read file and create a title from it based on a set of conditions
 	
 	//Save the file in a common folder with the generated name
