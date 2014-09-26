94e90a706f344718accd1647d3676b99bc689823
purvesta <purvesta0704@gmail.com>
2014-09-26 14:38:49 -0600

    deleted ajax.js and added to global.js

--- inc/global.js
+++ inc/global.js
@@ -34,16 +34,16 @@
 		
 		
 		if(choice1.is(':checked')){						//If choice one is selected, then an alert is sent and page is reloaded.
-			alert('You chose the Pie Chart.');
 			location.reload();
+			return 1;
 		}
 		else if(choice2.is(':checked')){				//If choice two is selected, then an alert is sent and page is reloaded.
-			alert('You chose the Line Graph.');			
 			location.reload();
+			return 2;
 		}
 		else if(choice3.is(':checked')){				//If choice three is selected, then an alert is sent and page is reloaded.
-			alert('You chose the Bar Graph.');
-			location.reload();	
+			location.reload();
+			return 3;	
 		}
 		else{											//If no choice is selected, then a error alert is sent and page is reloaded.
 			alert('You need to choose a graph...');	
@@ -51,5 +51,119 @@
 		}
 		
 		
+	}
+	
+	//Ajax javascript starts here!!
+	
+	//Need to set correct file url in the ajax function
+	//Three remaining variables to get from the csv file -- need to figure out which element of which array they will be, then set them accordingly, likley in the processData function
+	var titleName = asdf;
+	var yAxisTitle = asdf;
+	
+	
+	//Define initial skeleton for chart
+	var options = {
+		chart:{
+			renderTo: 'container',
+			defaultChartType: 'column',
+			type: chartType
+		},
+		title:{
+			text: titleName
+		},
+		xAxis:{
+			categories: []
+		},
+		yAxis:{
+			title:{
+				text: yAxisTitle	
+			}
+		},
+		series: []
+	};
+	
+	function processData(data){
+		//Split the rows
+		var dataRows = data.split("\n");
+	
+		//Iterate over the lines and add categories or series
+		$.each(lines, function(lineNum, line){
+			var items = line.split(',');	
+		
+			//header line contains categories
+			if(lineNum == 0){
+				$.each(items, function(itemNum, value){
+					if(itemNum > 0){
+						options.xAxis.categories.push(value);
+					}
+				});
+			}else{
+				//The rest of the lines contain data with their name in the first position
+				var series = {
+						data: []
+				};
+				$.each(items, function(itemNum, value){
+						if(itemNum == 0){
+							series.name = value;
+						}else{
+							series.data.push(parseFloat(value));	
+						}
+				});
+				
+				options.series.push(series);
+				
+			}
+		});
+		//Create the chart
+		var chart = new Highcharts.Chart(options);
+	}
+		
+		//Create chart
+	//Change ready function to on submit/when the php gets done
+	if(validate() == 1){
+		
+		var chartType = 'pie';
+	
+		$.ajax({ 
+		
+		
+			type: "POST", 
+			url: "../index.html",		//Need to add the correct url
+			dataType: "csv", 
+			success: function(data){
+				processData(data);
+			}
+		});
+	
+	}
+	else if(validate() == 2){
+		var chartType = 'line';
+		
+		$.ajax({ 
+		
+		
+			type: "POST", 
+			url: "../index.html",		//Need to add the correct url
+			dataType: "csv", 
+			success: function(data){
+				processData(data);
+			}
+		});
+		
+	}
+	
+	else if(validate() == 3){
+		var chartType = 'bar';
+		
+		$.ajax({ 
+		
+		
+			type: "POST", 
+			url: "../index.html",		//Need to add the correct url
+			dataType: "csv", 
+			success: function(data){
+				processData(data);
+			}
+		});
 	}
 		
