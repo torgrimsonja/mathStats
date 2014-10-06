2541ec9e2d4d96496cc02896cfdc3120ad3ce3ac
Joel Christensen <christensenjo14@gmail.com>
2014-10-06 14:55:16 -0600

    Worked on Server Side Stuff and Added Uploads folder
    See file in uploads folder for more info on that

--- inc/serverSide.php
+++ inc/serverSide.php
@@ -1,3 +1,4 @@
+<!-- Need to figure out how to run this script once the file has been uploaded in conjunction with the jquery stuff -->
 <?php
 	//The purpose of this file is to allow the MathStats App to save CSV files so that charts may be recreated with them at a later time
 	
@@ -5,18 +6,19 @@
 		$fileUpload = array();
 		$fileUpload[]= $_FILES[];
 		
-		if ($fileUpload == "csv"){
+		 if($_FILES['file']['type'] == "csv"){
 			//Make sure the file type is csv
-			    if (file_exists("".$fileUpload["name"]."")) {
+			    if (file_exists("".$_FILES["file"]["tmp_name"]."")) {
 					  echo $fileUpload."already exists. ";
 				}else{
-					  move_uploaded_file($_FILES["file"]["tmp_name"],
-					  "upload/" . $_FILES["file"]["name"]);
-					  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+					  //move_uploaded_file(string $filename, string $destination);
+					  //Will need to make sure that this function is pointed to the correct location and that the uploads folder can simply be local... it probably cant
+					  move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $_FILES["file"]["name"]);
+					  echo "Uploading: " . $_FILES['file']['name'] . "<br />";
 				}
 		 }else{
 			//Deal with non-csv files
-			echo "Invalid file";
+			echo "Invalid file, please try again...";
 		 }
 	//Read file and create a title from it based on a set of conditions
 	
@@ -24,5 +26,4 @@
 	
 	//Set up Load File functionality
 	
-
 ?>
