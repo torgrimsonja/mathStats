11932d59f3c5317f868a2afc02655713dfddc223
Joel Christensen <christensenjo14@gmail.com>
2014-10-07 14:49:41 -0600

    More File Saving Capability Work

--- inc/serverSide.php
+++ inc/serverSide.php
@@ -1,29 +1,41 @@
 <!-- Need to figure out how to run this script once the file has been uploaded in conjunction with the jquery stuff -->
 <?php
-	//The purpose of this file is to allow the MathStats App to save CSV files so that charts may be recreated with them at a later time
+	//The purpose of this file is to allow the MathStats App to save CSV files so that charts may be loaded for reuse at a later time
+	error_reporting(E_ALL);
 	
 	//Get file from html form upload
-		$fileUpload = array();
-		$fileUpload[]= $_FILES[];
-		
+	$uploadPath = "mathStats/uploads/";
+					  
 		 if($_FILES['file']['type'] == "csv"){
 			//Make sure the file type is csv
-			    if (file_exists("".$_FILES["file"]["tmp_name"]."")) {
-					  echo $fileUpload."already exists. ";
+			    if (file_exists("".$_FILES["file"]["name"]."")) {
+					 echo $_FILES["file"]["name"]."already exists, an underscore and incremented number will be added to the end of the file name.";
+					 
+					 $fileIncrement = while(file_exists($_FILES["file"]["tmp_name"])){
+					  };
+					 
+					 $_FILES["file"]["name"] = $_FILES["file"]["tmp_name"]."_".$fileIncrement;
+					  
+					  //For multiple file uploads on the same day, have it add an underscore and increment the file name
+					  //EX:  upload1 == 9/5/14 && upload2 == 9/45/14_1
 				}else{
 					  //move_uploaded_file(string $filename, string $destination);
 					  //Will need to make sure that this function is pointed to the correct location and that the uploads folder can simply be local... it probably cant
-					  move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $_FILES["file"]["name"]);
+					  //Upload file to proper location with a name that is based off of the date of the upload
+					  $_FILES["file"]["name"] == 
+					  move_uploaded_file($_FILES["file"]["tmp_name"], "mathStats/uploads"); //Location needs changed
 					  echo "Uploading: " . $_FILES['file']['name'] . "<br />";
+					  if(file_exists($uploadPath . $_FILES["file"]["name"])){
+						 echo "Upload successful.";
+					  }else{
+						 echo "Upload failed.";
+					  }
 				}
 		 }else{
 			//Deal with non-csv files
 			echo "Invalid file, please try again...";
 		 }
-	//Read file and create a title from it based on a set of conditions
-	
-	//Save the file in a common folder with the generated name
-	
+		 
 	//Set up Load File functionality
 	
 ?>
