38d38e3280b729b8eee80109f5ae81d6397b5b73
The Slag <quentinslagowski@gmail.com>
2014-11-03 14:20:36 -0700

    cut out lots of white space

--- inc/global.js
+++ inc/global.js
@@ -1,37 +1,26 @@
 // JavaScript Document
 	$(document).on('pageinit', function() {
 		console.log('Ready event fired');
-		
 		//$('#fileSubmit').bind('click', clicked());
-		
 		dothis();
 	});
-	
 	//This function will be used to submit file
 	function dothis(){
 		$("#help").click(function(){
 			alert("Choose your file and graph type. Next click submit and watch as your graph is printed out on screen.");	
 		});
-		
 		$( "#graphChoice" ).submit(function( event ) {
 			console.log( "Handler for .submit() called." );
 			event.preventDefault();
 			validate();
 		});
-		
-		
-	
 	}
-	
 	function checkName(){
 		var filePath = document.getElementById("fileSelect").value();
 		var fileName = filePath.replace(/^.*[\\\/]/, '')
 		$.ajax({ 
-		
-		
 			type: "GET", 
 			url: "../ajax.php?action=checkName&name="+fileName,		
-			
 			success: function(result){
 				if(result == 'Invalid'){
 					document.getElementById('errorFileName').style.display="inline";
@@ -40,17 +29,13 @@
 			}
 		});
 	}
-	
 	//This funtion will check to see if a graph type is selected
 	function validate(){
 		console.log('In validate()');
-
 		var selected = $('#fileSelect').val();
 		var choice1 = $("#choice1");
 		var choice2 = $("#choice2");
 		var choice3 = $("#choice3");
-		
-		
 		if(choice1.is(':checked')){						//If choice one is selected, then an alert is sent and page is reloaded.
 			location.reload();
 			return 1;
@@ -67,16 +52,10 @@
 			alert('You need to choose a graph...');	
 			location.reload();
 		}
-		
-		
 	}
-	
-	
 	//Title and yAxis title still need to be defined!!!!!!!!!!!!!!!!!!!!
 	var titleName = asdf/*Date uploaded*/;
 	var yAxisTitle = "Temperature";
-	
-	
 	//Define initial skeleton for chart
 	var options = {
 		chart:{
@@ -104,15 +83,12 @@
 					data:[]
 				}]
 	};
-	
 	function processData(data){
 		//Split the rows
 		var dataRows = data.split("\n");
-	
 		//Iterate over the lines and add categories or series
 		$.each(lines, function(lineNum, line){
 			var items = line.split(',');	
-		
 			//header line contains categories
 			if(lineNum == 0){
 				$.each(items, function(itemNum, value){
@@ -132,57 +108,41 @@
 							series.data.push(parseFloat(value));	
 						}
 				});
-				
 				options.series.push(series);
-				
 			}
 		});
 		//Set the code for the chart to a variable
 		var chart = new Highcharts.Chart(options);
-	}
-		
+	}	
 	//Create chart
 	//Run this code once the csv file has been submitted and a chart type chosen
 	if(validate() == 1){
-		
 		var chartType = 'pie';
-	
-		$.ajax({ 
-		
-		
+		$.ajax({
 			type: "POST", 
-			url: "../index.html",		//Need to add the correct url
+			url: "main.php",		//Need to add the correct url
 			dataType: "text", 
 			success: function(data){
 				processData(data);
 			}
 		});
-	
 	}
 	else if(validate() == 2){
 		var chartType = 'line';
-		
 		$.ajax({ 
-		
-		
 			type: "POST", 
-			url: "../index.html",		//Need to add the correct url
+			url: "main.php",		//Need to add the correct url
 			dataType: "text", 
 			success: function(data){
 				processData(data);
 			}
 		});
-		
 	}
-	
 	else if(validate() == 3){
 		var chartType = 'bar';
-		
 		$.ajax({ 
-		
-		
 			type: "POST", 
-			url: "serverSide.php",		//Need to add the correct url
+			url: "main.php",		//Need to add the correct url
 			dataType: "text", 
 			success: function(data){
 				processData(data);
@@ -192,14 +152,9 @@
 						//chart.print(options);
 					});
 				});
-
 			}
 		});
-		
 		$('#container').highcharts({
 			
    	 	});
 	}
-		
-
-
