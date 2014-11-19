1a958f73af4513a99500c05c30599253a5c5037c
purvesta <purvesta0704@gmail.com>
2014-11-19 14:43:37 -0700

    Torgrimson-splosion
    He just dominated

---
+++ inc/ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+//Handle save file name check
+if(	array_key_exists('action', $_GET) &&
+	$_GET['action'] == 'checkName' &&
+	array_key_exists('name', $_GET)
+	){
+	
+	//look at the folder to see if a file with that name exists
+	if(file_exists("uploads/".$_GET['name'])){
+	   echo 'Invalid';
+	}else{
+		echo 'Valid';	
+	}
+	exit();
+}
+
+//Handle loading of previously saved files for chart creation
+
+
