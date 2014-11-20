29c4c5c72999bda1864b49c871c80ed49e2dc94a
purvesta <purvesta0704@gmail.com>
2014-11-20 14:34:17 -0700

    added stuff
    asdf

--- inc/ajax.php
+++ inc/ajax.php
@@ -2,17 +2,17 @@
 
 //Handle save file name check
 if(	array_key_exists('action', $_GET) &&
-	$_GET['action'] = 'checkName' &&
+	$_GET['action'] == 'checkName' &&
 	array_key_exists('name', $_GET)
 	){
 	
 	//look at the folder to see if a file with that name exists
 	if(file_exists("uploads/".$_GET['name'])){
-	   echo "Invalid";
+	   echo 'Invalid';
+	}else{
+		echo 'Valid';	
 	}
+	exit();
 }
 
 //Handle loading of previously saved files for chart creation
-
-
-?>
