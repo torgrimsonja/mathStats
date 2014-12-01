7ea3cb0477dd696d589689d7b662fd8b0b99429c
The Slag <quentinslagowski@gmail.com>
2014-12-01 14:42:16 -0700

    Coded the buttons

--- inc/global.js
+++ inc/global.js
@@ -155,3 +155,27 @@
 		var chartType = 'bar';
 		
 	}
+	
+	//Export Module Code
+	function Export(){
+		var e = document.getElementById("ExportOption");
+        var ExportAs = e.options[e.selectedIndex].value;   
+        
+        if(ExportAs == 'PNG')
+        {
+            chart.exportChart({type: 'image/png', filename: 'my-png'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'JPEG')
+        {
+            chart.exportChart({type: 'image/jpeg', filename: 'my-jpg'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'PDF')
+        {
+            chart.exportChart({type: 'application/pdf', filename: 'my-pdf'}, {subtitle: {text:''}});
+        }
+        if(ExportAs == 'SVG')
+        {
+            chart.exportChart({type: 'image/svg+xml', filename: 'my-svg'}, {subtitle: {text:''}});
+        }
+	}
+	$('buttonExport').click(Export());
